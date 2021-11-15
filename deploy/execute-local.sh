cd3a71643f8265d804722f71a59003f3ee6bfa9b
RjRDRG <rj.ribeiro@campus.fct.unl.pt>
2021-11-15 22:11:31 +0000

    bug fix

--- deploy/execute-local.sh
+++ deploy/execute-local.sh
@@ -6,4 +6,4 @@ index=$(($3 + 1))
 pwd=$4
 
 cd $pwd
-(java -jar target/asdProj.jar -conf config.properties interface=bond0 port=$port contact=$contactaddr my_index=$index</dev/null >results/results-$(hostname)-$port.txt 2>&1)&
+(java -jar target/asdProj.jar -conf babel_config.properties interface=bond0 port=$port contact=$contactaddr my_index=$index</dev/null >results/results-$(hostname)-$port.txt 2>&1)&
