77484f7f59bbb480789da57aa524a84654df543c
RjRDRG <rj.ribeiro@campus.fct.unl.pt>
2021-11-15 23:35:16 +0000

    bug fix

--- deploy/start-processes-cluster.sh
+++ deploy/start-processes-cluster.sh
@@ -20,7 +20,9 @@ contactaddr=$(ifconfig bond0 | awk '/inet / {print $2}'):$port
 while [ $i -lt $processes ]
 do
 	for node in $(oarprint host); do
-		oarsh $node "cd $pwd; nohup ./deploy/execute-local.sh $contactaddr $[$port+$i] $i $pwd &"
-	    i=$[$i+1]
-        done	  
+	  if [ $i -lt $processes ]; then
+		  oarsh $node "cd $pwd; nohup ./deploy/execute-local.sh $contactaddr $[$port+$i] $i $pwd &"
+      i=$[$i+1]
+    fi
+  done
 done
